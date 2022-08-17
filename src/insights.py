d4fd94f20631a322ebb34470f24594fbfd3991c6
Renato Adorno <renattoadorno@gmail.com>
2022-08-17 18:31:15 -0300

    ✨ feat: add get_min_salary

--- src/insights.py
+++ src/insights.py
@@ -64,22 +64,15 @@ def get_max_salary(path: str):
     return max(salaries)
 
 
-def get_min_salary(path):
-    """Get the minimum salary of all jobs
+def get_min_salary(path: str):
+    jobs = read(path)
+    salaries = [
+        int(job["min_salary"])
+        for job in jobs
+        if job["min_salary"].isdigit()
+        ]
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    return min(salaries)
 
 
 def matches_salary_range(job, salary):
