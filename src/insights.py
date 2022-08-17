ebab06120aab35c34dbb7bae474e428a0908fec8
Renato Adorno <renattoadorno@gmail.com>
2022-08-17 18:22:05 -0300

    ✨ feat: add get_unique_industries

--- src/insights.py
+++ src/insights.py
@@ -26,22 +26,13 @@ def filter_by_job_type(jobs, job_type):
     return []
 
 
-def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+def get_unique_industries(path: str):
+    jobs = read(path)
+    industries = set()
+    for job in jobs:
+        if job["industry"] != "":
+            industries.add(job["industry"])
+    return list(industries)
 
 
 def filter_by_industry(jobs, industry):
