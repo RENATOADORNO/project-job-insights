ab06927f78770bb05d25c65a3e85929c63d5c6aa
Renato Adorno <renattoadorno@gmail.com>
2022-08-18 07:52:11 -0300

    ✨ feat: add filter_by_industry

--- src/insights.py
+++ src/insights.py
@@ -26,21 +26,11 @@ def get_unique_industries(path: str):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    filtered = []
+    for job in jobs:
+        if job['industry'] == industry:
+            filtered.append(job)
+    return filtered
 
 
 def get_max_salary(path: str):
