29ec0298d427295c100de42da4a8e23fc96b0e67
Renato Adorno <renattoadorno@gmail.com>
2022-08-19 15:41:55 -0300

    🧪 test: add test count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,25 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
+from unittest.mock import mock_open, patch
+import pytest
 
 
-def test_counter():
-    pass
+@pytest.fixture
+def jobs():
+    return [
+        {"title": "Front end developer", "salary": "2000", "type": "trainee"},
+        {"title": "Back end developer", "salary": "3000", "type": "full time"},
+        {
+            "title": "Full stack end developer",
+            "salary": "4000",
+            "type": "full time",
+        },
+    ]
+
+
+def test_counter(jobs):
+    with patch("builtins.open", mock_open(read_data=str(jobs))):
+        normal_counter = count_ocurrences("src/jobs.csv", "developer")
+        assert normal_counter == 3
+
+        sensitive_counter = count_ocurrences("src/jobs.csv", "dEvELoPER")
+        assert sensitive_counter == 3
