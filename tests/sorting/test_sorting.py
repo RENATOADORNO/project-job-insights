2a66a6b9ddc4ea8158db1dd21baecabe7ff160ef
Renato Adorno <renattoadorno@gmail.com>
2022-08-19 16:55:12 -0300

    🧪 test: add test sorting

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,25 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+
+SALARY_CRITERIAS = ["min_salary", "max_salary"]
+
+SALARY = [
+    {"max_salary": 9000, "min_salary": 2500},
+    {"max_salary": 999, "min_salary": 1000},
+    {"max_salary": 15000, "min_salary": 500},
+    {"max_salary": 5, "min_salary": 2},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    for criteria in SALARY_CRITERIAS:
+        sort_by(SALARY, criteria)
+        if criteria == "min_salary":
+            assert (SALARY[0][criteria] <= SALARY[1][criteria])
+            assert (SALARY[1][criteria] <= SALARY[2][criteria])
+            assert (SALARY[2][criteria] <= SALARY[3][criteria])
+
+        else:
+            assert (SALARY[0][criteria] >= SALARY[1][criteria])
+            assert (SALARY[1][criteria] >= SALARY[2][criteria])
+            assert (SALARY[2][criteria] >= SALARY[3][criteria])
